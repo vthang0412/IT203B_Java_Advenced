dc3a09ee69ba97316f55a9450982c7ee7730b3f5
cout "DangVanThangendl <dvthang0412@gmail.com>
2026-03-22 22:19:33 +0700

    Delete src/Session10 directory

--- src/Session10/Btth/utils/Main.java
+++
@@ -1,9 +0,0 @@
-package Session10.Btth.utils;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Chuẩn bị kết nối");
-        DatabaseConnection.openConnection();
-        System.out.println("Đã kết nối thành công");
-    }
-}
